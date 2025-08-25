b0d3d5634e2d318e4427321451ee54931917bb6b
OdhiamboGeoffrey <geoffrey.ochieng.odhiambo03@gmail.com>
2025-08-25 21:22:04 +0300

    Trial 1
    Tried to implement the algorithm using binary search but no solution, Keep Moving Forward

--- qtn1.py
+++ qtn1.py
@@ -1,3 +1,12 @@
+# you are given a list of numbers obtained by rotating a sorted
+# list an unknown number of times. You are also given a target
+# number. Write a function to find the position of the target
+# number within the rotated list. You can assume that all the numbers
+# in the list are unique
+# [5, 6, 9, 0, 2, 3, 4] the target number is 2 and occurs at position
+#  5
+
+
 
 tests = []
 
@@ -10,10 +19,10 @@ tests.append({
 })
 tests.append({
     'input' :{
-        'nums' : [6, 6, 6, 6, 6, 5, 5, 4, 3, 8 ,9, 11, 12],
+        'nums' : [8 ,9, 11, 12, 3, 4, 4, 4,  4, 4, 5, 5, 5],
         'target' : 4
     },
-    'output' : 7
+    'output' : 5
 })
 tests.append({
     'input' :{
@@ -24,7 +33,7 @@ tests.append({
 })
 tests.append({
     'input' :{
-        'nums' : [7, 8, 9 ,2, 3, 4, 4, 6],
+        'nums' : [7, 8, 9 ,2, 3, 4, 4, 4, 6],
         'target' : 4
     },
     'output' : 5
@@ -58,27 +67,34 @@ tests.append({
     'output' : 4
 })
 
+nums = [5, 6, 7, 8, 9, 1, 2, 3, 4, 5]
+target = 2
+output = 6
 
 def find_element(nums, target):
-    position = 0
+    lo, hi = 0, len(nums) - 1
 
-    while position < len(nums):
-        if nums[position] == target:
-            return position
-        
-        position += 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        mid_number = nums[mid]
+        print(mid_number)
+
+        if target == mid_number:
+            return mid
+        elif target < nums[hi]:
+            lo = mid + 1
 
     return 0
 
 
-for i in range(len(tests)):
-    nums = tests[i]['input']['nums']
-    target = tests[i]['input']['target']
-    output = tests[i]['output']
-    result = find_element(nums, target)
-    is_correct = result == output
+# for i in range(len(tests)):
+#     nums = tests[i]['input']['nums']
+#     target = tests[i]['input']['target']
+#     output = tests[i]['output']
+#     result = find_element(nums, target)
+#     is_correct = result == output
 
-    print(f'Test {i}: {is_correct} - target: {target}, output: {result}, expected: {output}')
+#     print(f'Test {i}: {is_correct} - target: {target}, output: {result}, expected: {output}')
 
-# result = find_element(nums, target)
-# print(result)
+result = find_element(nums, target)
+print(f'output: {result}')
