52067d5d0775d6c1f5eff8c9bb1b0441c898e2c8
OdhiamboGeoffrey <geoffrey.ochieng.odhiambo03@gmail.com>
2025-08-28 21:36:13 +0300

    Practice
    Practicing what was learned about BST, searching, inserting, deleting and traversing

--- practice.py
+++ practice.py
@@ -30,39 +30,94 @@ def postOrderTraversal(node):
     postOrderTraversal(node.right)
     print(node.data, end=', ')
 
+def search(node, target):
+    if node is None:
+        return None
+    elif node.data == target:
+        return node
+    elif target < node.data:
+        return search(node.left, target)
+    else:
+        return search(node.right, target)
+
+def insert(node, data):
+    if node is None:
+        return TreeNode(data)
+    else:
+        if data < node.data:
+            node.left = insert(node.left, data)
+        elif data > node.data:
+            node.right = insert(node.right, data)
+    return node
+
+def minValueNode(node):
+    current = node
+    while current.left is not None:
+        current = current.left
+    return current
+
+def delete(node, data):
+    if not node:
+        return None
+    
+    if data < node.data:
+        node.left = delete(node.left, data)
+    elif data > node.data:
+        node.right = delete(node.right, data)
+    else:
+        # Node with only one child or no child
+        if not node.left:
+            temp = node.right
+            node = None
+            return temp
+        elif not node.right:
+            temp = node.left
+            node = None
+            return temp
+        
+        # Node with two children, get the in-order successor
+        node.data = minValueNode(node.right).data
+        node.right = delete(node.right, node.data)
+
+    return node
 
 # creating the nodes
-root = TreeNode('R')
-nodeA = TreeNode('A')
-nodeC = TreeNode('C')
-nodeD = TreeNode('D')
-nodeB = TreeNode('B')
-nodeE = TreeNode('E')
-nodeF = TreeNode('F')
-nodeG = TreeNode('G')
+root = TreeNode(13)
+node7 = TreeNode(7)
+node3 = TreeNode(3)
+node8 = TreeNode(8)
+node15 = TreeNode(15)
+node14 = TreeNode(14)
+node19 = TreeNode(19)
+node18 = TreeNode(18)
 
 
 # Creating the Binary Tree
-root.left = nodeA
-root.right = nodeB
+root.left = node7
+root.right = node15
 
-nodeA.left = nodeC
-nodeA.right = nodeD
+node7.left = node3
+node7.right = node8
 
-nodeB.left = nodeE
-nodeB.right = nodeF
+node15.left = node14
+node15.right = node19
 
-nodeF.left = nodeG
+node19.left = node18
 
-print('\npreOrderTraversal of the Tree')
-preOrderTraversal(root)
+# Searching for a node
+# search_result = search(root, 0)
+# print("\nSearch result:", search_result.data if search_result else "Not found") #tenary operator
 
-print('')
+# Inserting a value
+# insert(root, 9)
+# print("In-order traversal after inserting 9:")
+# inOrderTraversal(root)
 
-print('\ninOrderTraversal of the Tree')
+# finding the minValueNode in the tree
+# minValueNode_result = minValueNode(root)
+# print("\nMinimum value node:", minValueNode_result.data if minValueNode_result else "Tree is empty")
+
+# Deleting a node
+delete(root, 14)
+print("In-order traversal after deleting 14:")
 inOrderTraversal(root)
-
-print('')
-
-print('\npostOrderTraversal of the Tree')
-postOrderTraversal(root)
