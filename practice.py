a791ea4ca12161574ec649f307081a8eff164c7c
OdhiamboGeoffrey <geoffrey.ochieng.odhiambo03@gmail.com>
2025-09-02 17:02:25 +0300

    Practice Session

--- practice.py
+++ practice.py
@@ -1,16 +1,34 @@
 import time as t
 
 
-nums = [3, 5, 6, 8, 1, 7, 4]
 
-nums.insert(0, 12)
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
 
+tree_tuple = ((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8)))
 
-print("Started")
-start_time = t.time()
-for i in range(100000000):
-    j = i * i
+def parse_tuple(data):
+    if isinstance(data, tuple) and len(data) == 3:
+        node = TreeNode(data[1])
+        node.left = parse_tuple(data[0])
+        node.right = parse_tuple(data[2])
+    elif data is None:
+        node = None
+    else:
+        node = TreeNode(data)
 
-end_time = t.time()
-print(f'Total time taken: {start_time - end_time}')
-print(nums)
+    return node
+
+def inOrderTraversal(node):
+    if node is None:
+        return
+    inOrderTraversal(node.left)
+    print(node.key, end=' ')
+    inOrderTraversal(node.right)
+
+tree = parse_tuple(tree_tuple)
+
+inOrderTraversal(tree)
