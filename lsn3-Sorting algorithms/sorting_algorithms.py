89f0dc2cd3d555f98a004be799fa0e7e8f81d756
OdhiamboGeoffrey <geoffrey.ochieng.odhiambo03@gmail.com>
2025-09-20 11:46:08 +0300

    Quick_sort algorithm and Partition
    the partition function returns the position of the pivot

--- lsn3-Sorting algorithms/sorting_algorithms.py
+++ lsn3-Sorting algorithms/sorting_algorithms.py
@@ -55,5 +55,50 @@ def merge_sort(nums: list[int]) -> list[int]:
 
     return sorted_nums
 
+def partition(nums: list[int], start=0, end=None) -> int:
+    # print('partition', nums, start, end)
+    if end is None:
+        end = len(nums) - 1
+
+    # initialize right and left pointers
+    l, r = start, end-1
+
+    # iterate while they're apart
+    while r > l:
+        # print(' ', nums, l, r)
+        # increament left pointer if number is less or equal to pivot
+        if nums[l] <= nums[end]:
+            l += 1
+        # decreament right pointer if number is greater than pivot
+        elif nums[r] > nums[end]:
+            r -= 1
+        # two out-of-place elements found, swap them
+        else:
+            nums[l], nums[r] = nums[r], nums[l]
+    
+    # print(' ', nums, l, r)
+    # place the pivot between the two parts
+    if nums[l] > nums[end]:
+        nums[l], nums[end] = nums[end], nums[l]
+        return l
+    else:
+        return end
+    
+def quick_sort(nums: list[int], start=0, end=None) -> list[int]:
+    print('quick_sort', nums, start, end)
+    if end is None:
+        nums = list(nums)
+        end = len(nums) - 1
+
+    if start < end:
+        pivot = partition(nums, start, end)
+        quick_sort(nums, start, pivot-1) #partition before the pivot
+        quick_sort(nums, pivot+1, end) #partition after the pivot
+
+    return nums
+
+# nums: list[int] = [1, 5, 4, 2, 0, 11, 3, 44, 7, 12, 6]
+# result = quick_sort(nums)
+# print(result)
 
 result = evaluate_test_case(merge_sort, test)
