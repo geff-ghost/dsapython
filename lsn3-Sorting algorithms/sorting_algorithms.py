88993f62c00d1402da19cf1979393d736716a0bc
Geff <geoffrey.ochieng.odhiambo03@gmail.com>
2025-11-07 14:59:58 +0300

    Quick Sort Implemetation

--- lsn3-Sorting algorithms/sorting_algorithms.py
+++ lsn3-Sorting algorithms/sorting_algorithms.py
@@ -1,5 +1,5 @@
 from tests import tests
-test = tests[7]
+test = tests[0]
 
 
 def bubble_sort(nums: list[int]):
@@ -28,7 +28,7 @@ def merge(left, right):
     i, j, merged = 0, 0, []
     
     while i < len(left) and j < len(right):
-        if left[i] < right[j]:
+        if left[i] <= right[j]:
             merged.append(left[i])
             i += 1
         else:
@@ -52,8 +52,44 @@ def merge_sort(nums):
     
     return merge(left_sorted, right_sorted)
 
+def partition(nums, start=0, end=None):
+    if end is None:
+        end = len(nums)-1
+    
+    l, r = start, end-1
+    
+    while r > l:
+        # increment the left pointer in number is less or equal to pivot
+        if nums[l] <= nums[end]:
+            l += 1
+        # decrement the right pointer if number is greater than pivot
+        elif nums[r] > nums[end]:
+            r -= 1
+        # two out of place elements found, swap them
+        else:
+            nums[l], nums[r] = nums[r], nums[l]
+    
+    # place the pivot between the two parts
+    if nums[l] > nums[end]:
+        nums[l], nums[end] = nums[end], nums[l]
+        return l
+    else:
+        return end
 
-sorted_arr = merge_sort(test['input']['nums'])
+def quick_sort(nums, start=0, end=None):
+    if end is None:
+        nums = list(nums)
+        end = len(nums)-1
+        
+    if start < end:
+        pivot = partition(nums, start, end)
+        quick_sort(nums, start, pivot-1)
+        quick_sort(nums, pivot+1, end)
+        
+    return nums
+
+
+sorted_arr = quick_sort(test['input']['nums'])
 
 print(f'Unsorted arr: {test["input"]['nums']}')
 print(f'Expected output: {test['output']}')
